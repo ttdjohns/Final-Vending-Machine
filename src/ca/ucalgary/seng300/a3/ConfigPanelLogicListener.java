36939d991c6549e24874429b0b007451cd98e386
AnthonyCoulthard <anthony.coulthard93@gmail.com>
2017-12-07 22:37:31 -0700

    Update ConfigPanelLogicListener.java
    Added comments.

--- src/ca/ucalgary/seng300/a3/ConfigPanelLogicListener.java
+++ src/ca/ucalgary/seng300/a3/ConfigPanelLogicListener.java
@@ -1,5 +1,12 @@
+/**
+*SENG 300 Group 3
+*/
+
 package ca.ucalgary.seng300.a3;
 
+/**
+* Listener class for the configuration panel.
+*/
 public class ConfigPanelLogicListener {
 	
 	private GUI mygui;
@@ -9,6 +16,11 @@ public class ConfigPanelLogicListener {
 		ConfigPanelLogic.getInstance().register(this);
 	}
 	
+	/**
+	* Method to update the price of a pop (the only function of the config panel for now).
+	* @param buttonIndex - the index of the pop button that was selected
+	* @param newPrice - the price the pop will get changed to
+	*/
 	public void priceUpdate(int buttonIndex, int newPrice) {
 		double newPopCost;
 		
